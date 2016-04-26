b21f57aea178a031dbcfb2e3d99cf1db307a378f
Cleber Fonseca <cleber@3wsolution.com.br>
2016-04-26 16:28:01 -0300

    Exercícios de funções

---
+++ funcoes.php
@@ -0,0 +1,29 @@
+<?php
+include('functions.inc');
+
+soma(10, 15);
+
+print("<br />--------------------<br />");
+
+divisao(10);
+
+print("<br />--------------------<br />");
+
+print(genero("Giovana", 17, "F"));
+
+print("<br />--------------------<br />");
+
+triangulo(20, 20, 20);
+
+print("<br />--------------------<br />");
+
+print(meses(1));
+
+print("<br />--------------------<br />");
+
+algoritmos(5);
+
+print("<br />--------------------<br />");
+
+tabuada(5);
+?>
