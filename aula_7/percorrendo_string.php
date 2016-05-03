a947f3daf16854c665b358d3cbdf925792cfbb7c
Cleber Fonseca <cleber@3wsolution.com.br>
2016-05-03 16:12:37 -0300

    Aula 7 - indices de string

--- aula_7/percorrendo_string.php
+++ aula_7/percorrendo_string.php
@@ -3,6 +3,6 @@ $my_string = "Doubled";
 
 for ($i=0; $i<7; $i++){
 	$string_to_print = $my_string{$i};
-	print("$string_to_print$string_to_print");
+	print("$string_to_print$string_to_print);
 }
 ?>
