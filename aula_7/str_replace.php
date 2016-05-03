27b578f1bb78cddf66322efc80ee5b7605636570
Cleber Fonseca <cleber@3wsolution.com.br>
2016-05-03 17:09:50 -0300

    Aula 7 - str_replace

---
+++ aula_7/str_replace.php
@@ -0,0 +1,13 @@
+<?php
+$str1 = "O professor Cleber ensina LPWI";
+
+print($str1);
+
+echo "<br />";
+
+print(str_replace("Cleber", "Fabio", $str1));
+
+echo "<br />";
+
+print(str_replace("LPWI", "SI", $str1));
+?>
