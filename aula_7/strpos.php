11d520ef84bae93659b3c4972b80100a421d282a
Cleber Fonseca <cleber@3wsolution.com.br>
2016-05-03 16:30:20 -0300

    Aula 7 - strpos

--- aula_7/strpos.php
+++ aula_7/strpos.php
@@ -4,4 +4,6 @@ $string = "Essa é uma string para ser usada nos exemplos.";
 print("A localização de 'm' é ".strpos($string, 'm'));
 echo "<br />";
 print("A localização de 'para' é ".strpos($string, 'para'));
+echo "<br />";
+print("A localização de 'm' no reverso é ".strrpos($string, 'm'));
 ?>
