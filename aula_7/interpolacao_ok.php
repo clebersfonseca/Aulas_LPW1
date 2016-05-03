4dc4023552b3aac6b3e36fe16f1463db23f517c2
Cleber Fonseca <cleber@3wsolution.com.br>
2016-05-03 15:06:28 -0300

    Aula 7 - Interpolação

--- aula_7/interpolacao_ok.php
+++ aula_7/interpolacao_ok.php
@@ -1,9 +1,9 @@
 <?php
 $sport1 = 'volley';
 $sport2 = 'foot';
-$soprt3 = 'basket';
+$sport3 = 'basket';
 
-echo "I will play {$sport1}ball in the summertime";
-echo "I will play {$sport2}ball in the fall";
-echo "I will play {$sport3}ball in the winter";
+echo "I will play {$sport1}ball in the summertime<br />";
+echo "I will play {$sport2}ball in the fall<br />";
+echo "I will play {$sport3}ball in the winter<br />";
 ?>
