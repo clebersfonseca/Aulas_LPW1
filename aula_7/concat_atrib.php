a0d4d95dcc857489d93651cd64dbe06871931ea0
Cleber Fonseca <cleber@3wsolution.com.br>
2016-05-03 16:03:01 -0300

    Aula 7 - concatenação e atribuição

---
+++ aula_7/concat_atrib.php
@@ -0,0 +1,16 @@
+<?php
+$first_str = "Essa é a primeira string";
+$first_str .= " e esse é a segunda string";
+
+echo $first_str;
+
+
+echo "<br />";
+
+
+//Mesmo que dizer
+$first_str = "Essa é a primeira string";
+$first_str = $first_str." e esse é a segunda string";
+
+echo $first_str;
+?>
---
+++ aula_7/operadores.php
@@ -0,0 +1,6 @@
+<?php
+$first_str = "Essa é a primeira string";
+$second_str = "e esse é a segunda string";
+
+print($first_str." ... ".$second_str);
+?>
